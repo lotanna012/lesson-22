364e61d52af52b1b2911019b75f0a8e431f690c6
lotanna012 <lotiaroh@gmail.com>
2025-08-22 10:49:40 +0100

    Add files via upload

--- num.py
+++ num.py
@@ -1,11 +1,11 @@
-numbers1=[1,2,3]
-numbers2=[4,5,6]
+numbers1=[4,5,6]
+numbers2=[7,8,9]
 def add (x,y):
     return x+y
 result=map(add,numbers1, numbers2)
 print("addition of two lists")
 print(list(result))
-nums=[1,2,3,4,5]
+nums=[3,4,5]
 def sq(n):
     return n*n
 square=map(sq,nums)
